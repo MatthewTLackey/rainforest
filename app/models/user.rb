8ae150bd762ec25d7cbd001ee43828afe4c44b2d
MatthewTLackey <lackey.matt@gmail.com>
2013-11-11 16:29:39 -0500

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   #validates_presence_of :password, :on => :create
   has_many :reviews
   has_many :products, :through => :reviews
+  validates_presence_of :name
 end
 
 
