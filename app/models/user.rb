365fdbdbdc37ab6f3bfc48e49d1f240688e91d3b
MatthewTLackey <lackey.matt@gmail.com>
2013-11-11 15:44:59 -0500

    Writing the review routes, controllers and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   #attr_accessor :password, :password_confirmation
   has_secure_password
   #validates_presence_of :password, :on => :create
+  has_many :reviews
+  has_many :products, :through => :reviews
 end
 
 
