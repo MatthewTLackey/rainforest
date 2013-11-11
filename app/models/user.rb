300bc10fa6f94831fa1af246320ad35bffe875f8
MatthewTLackey <lackey.matt@gmail.com>
2013-11-11 13:39:20 -0500

    Creating log-in functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,9 @@ class User < ActiveRecord::Base
   has_secure_password
   #validates_presence_of :password, :on => :create
 end
+
+
+#in-class
+# has_many :reviews
+# has_many :products, :through => :reviews
+# validates_presence_of :name
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rainforest::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   get "users/new"
   get "users/create"
   resources :products
   root :to => "products#index"
 
-   resources :users, :only => [:new, :create]
+   resources :users, :only => [:new, :create, :destroy]
+   resources :sessions, :only => [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
